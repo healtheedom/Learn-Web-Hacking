5b2142e3482354f50b6c3592890b5d19936bf21e
healtheedom <48197204+healtheedom@users.noreply.github.com>
2020-10-02 02:48:46 +0800

    Add files via upload

---
+++ python/post_request.py
@@ -0,0 +1,20 @@
+import sys
+import requests
+
+def main():
+	if len(sys.argv) != 2:
+		print "(+) usage %s <target>" % sys.argv[0]
+		print "(+) eg: %s target" % sys.argv[0]
+		sys.exit(1)
+	t = sys.argv[1]
+	# Passing a dictionary
+    dict_data = {'user': 'admin', 'pass': 'admin'}
+    r = requests.post('https://%s/login.php' % t , data = dict_data)
+	# Passing a string 
+    #headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+    #str_data = "user=admin&pass=admin;"    
+	#r = requests.post('https://%s/login.php' % t , headers = headers, data = str_data)
+	
+
+if __name__ == '__main__':
+	main()
